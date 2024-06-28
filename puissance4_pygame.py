d6b247e998097ac202e795f18c376676a2f12d84
Nathan-14-05 <emmanuelnathan.mboueya@uphf.fr>
2024-06-28 20:55:22 +0200

    Initial commit

---
+++ puissance4_pygame.py
@@ -0,0 +1,9 @@
+import pygame
+from sys import exit
+
+# Initialisation de pygame et création de la fenêtre
+pygame.init()
+largeurEcran = 700
+hauteurEcran = 800
+fenetre = pygame.display.set_mode((largeurEcran, hauteurEcran))
+pygame.display.set_caption("Puissance 4")
